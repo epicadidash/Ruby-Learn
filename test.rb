1db40c0a5cbf0654837517cf0c4d2537f13f3cd5
epicadidash <dradwaitdash@gmail.com>
2023-10-09 22:15:05 +0530

    Hashes in Ruby

--- test.rb
+++ test.rb
@@ -1,5 +1,4 @@
-
-ary = [  "fred", 10, 3.14, "This is a string", "last element", ]
-ary.each do |i|
-   puts i
+hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hsh.each do |key, value|
+   print key, " is ", value, "\n"
 end
