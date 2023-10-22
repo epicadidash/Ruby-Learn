231259b5463b98ff70ae16ecfd8985f8d1e23e51
epicadidash <dradwaitdash@gmail.com>
2023-10-22 22:56:20 +0530

    Student class

--- test.rb
+++ test.rb
@@ -1,4 +1,20 @@
-hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
-hsh.each do |key, value|
-   print key, " is ", value, "\n"
+class Student 
+  def initialize(id, name, addr)
+     @stud_id = id
+     @stud_name = name
+     @stud_addr = addr
+  end
+  def display_details()
+    puts "student id #@stud_id"
+    puts "student name #@stud_name"
+     puts "student address #@stud_addr"
+  end
 end
+
+# Create Objects
+stud1 = studomer.new("1", "John", "Wisdom Apartments, Ludhiya")
+stud2 = studomer.new("2", "Poul", "New Empire road, Khandala")
+
+# Call Methods
+stud1.display_details()
+stud2.display_details()
